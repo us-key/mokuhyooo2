95f7f947a40457de82f3949709674eb28e483dd8
us-key <y.omt0601@gmail.com>
2019-04-25 00:15:11 +0900

    modify setting

--- proj/settings.py
+++ proj/settings.py
@@ -26,37 +26,7 @@ SECRET_KEY = '^v#p5rjjg)-@m_j)0h97_5brf4hama%l$79m6vih4+@31e0lpz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-LOGGING = {
-    'disable_existing_loggers': False,
-    'version': 1,
-    'handlers': {
-        'console': {
-            # logging handler that outputs log messages to terminal
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG', # message level to be written to console
-        },
-    },
-    'loggers': {
-        '': {
-            # this sets root level logger to log debug and higher level
-            # logs to console. All other loggers inherit settings from
-            # root level logger.
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False, # this tells logger to send logging message
-                                # to its parent (will send if set to True)
-        },
-        'django.db': {
-            # django also has database level logging
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
-}
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
