a4c25d60390ac2549469f188179a8092282fe5f3
us-key <y.omt0601@gmail.com>
2019-06-07 08:57:00 +0900

    メッセージの出力対象絞り込み

--- templatetags/cmn_filter.py
+++ templatetags/cmn_filter.py
@@ -3,6 +3,23 @@ from django import template
 register = template.Library()
 
 @register.filter
+def obj_rev_msg_flg(key, val):
+    '''目標・振り返り作成のメッセージを出すか判定する'''
+    if val == "1":
+        # 目標：目標入力済
+        # 振り返り：目標入力済＆振り返り入力済
+        return False
+    else:
+        # 目標：未入力
+        # 振り返り：目標入力済＆振り返り未入力
+        if key[:1] == "T" and key[2:] == "R":
+            # 対象が今年・月・週・日かつ振り返りの場合はメッセージを出さない
+            return False
+        else:
+            return True
+
+
+@register.filter
 def create_obj_rev_msg(key):
     '''目標・振り返り作成のメッセージを作成する'''
     kind_dic = {"Y":"年","M":"月","W":"週","D":"日"}
